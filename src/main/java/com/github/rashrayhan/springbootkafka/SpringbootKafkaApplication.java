962bf30e824db0fbc396680ac03e61c1a19174db
rashrayhan <rashrayhan@gmail.com>
2021-06-20 03:05:29 -0500

    init commit

---
+++ src/main/java/com/github/rashrayhan/springbootkafka/SpringbootKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.github.rashrayhan.springbootkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKafkaApplication.class, args);
+	}
+
+}
